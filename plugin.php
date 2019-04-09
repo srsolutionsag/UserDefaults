748dc3849dc9cd25257b3480923afef82f32dc93
mstuder <ms@studer-raimann.ch>
2019-04-09 16:50:50 +0200

    set plugin version to 5.4

--- plugin.php
+++ plugin.php
@@ -5,6 +5,6 @@ require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = 'usrdef';
 $version = '1.3.20';
 $ilias_min_version = '5.2';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
 $responsible_mail = 'support-custom1@studer-raimann.ch';

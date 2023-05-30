fc8853ee3dac26c0fa6544231be6f28333eada6a
Martin Studer <martin@fluxlabs.ch>
2023-05-30 05:24:43 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,8 +3,6 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'usrdef';
-$version = "2023.02.16";
+$version = "2023.05.30";
 $ilias_min_version = '6.0';
 $ilias_max_version = '7.999';
-$responsible = 'studer + raimann ag - Team Custom 1';
-$responsible_mail = 'support-custom1@studer-raimann.ch';
